556ad965f7062c72103766f4570e133381988609
sourav16716 <54978571+sourav16716@users.noreply.github.com>
2025-09-03 16:55:41 +0530

    update apply job design
    update apply job design

--- app/featured-jobs-details/page.js
+++ app/featured-jobs-details/page.js
@@ -1,13 +1,20 @@
-import { Button } from 'flowbite-react';
+"use client";
+
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, FileInput, Label } from "flowbite-react";
+import { useState } from "react";
 import Link from 'next/link'
 import React from 'react'
 
 import { CgArrowLeft } from "react-icons/cg";
+import { BiImport } from "react-icons/bi";
 
 import small_inner_logo from "../assets/imagesource/small_inner_logo.png";
+import resume01 from "../assets/imagesource/resume01.png";
 import Image from 'next/image';
 
+
 const page = () => {
+  const [openJobApplyModal, setOpenJobApplyModal] = useState(true);
   return (
     <div className='bg-[#ffffff] rounded-[10px] p-10'>
         <div className='flex gap-2 items-center mb-8'>
@@ -26,10 +33,10 @@ const page = () => {
                       <div>
                         <h3 className='text-black text-[22px] leading-[24px] font-semibold pb-2'>Hiring Eye</h3>
                         <p className='text-[#6C6C6C] font-medium text-[15px] leading-[24px] pb-3'>Downtown District, USA</p>
-                        <div className='flex gap-2'>
+                        {/* <div className='flex gap-2'>
                             <div className='text-[#92278F] text-[14px] leading-[35px] rounded-[3px] px-5 bg-[#FFE8FE]'>Logistics</div>
                             <div className='text-[#92278F] text-[14px] leading-[35px] rounded-[3px] px-5 bg-[#FFE8FE]'>Hospitality</div>
-                        </div>
+                        </div> */}
                       </div>
                     </div>
                     <div className='w-6/12 flex gap-6 justify-end'>
@@ -52,7 +59,7 @@ const page = () => {
                   <div className='w-6/12 flex justify-end'>
                      <div>
                         <p className='text-[#626262] text-[14px] leading-[20px] pb-3 text-right'>Posted 3 days ago</p>
-                        <button className='bg-[#800080] hover:bg-[#151515] cursor-pointer px-10 text-[15px] leading-[45px] text-[#ffffff] font-semibold block text-center rounded-[7px]'>Apply Job</button>
+                        <button onClick={() => setOpenJobApplyModal(true)} className='bg-[#800080] hover:bg-[#151515] cursor-pointer px-10 text-[15px] leading-[45px] text-[#ffffff] font-semibold block text-center rounded-[7px]'>Apply Job</button>
                      </div>
                   </div>
                </div>
@@ -90,6 +97,69 @@ const page = () => {
                </ul>
             </div>
         </div>
+        {/* add modal for apply job start here */}
+          <Modal size="7xl" className="apply_modal_area" show={openJobApplyModal} onClose={() => setOpenJobApplyModal(false)}>
+                <ModalHeader className='bg-white text-black modal_header'>
+                  Upload Resume or Select Resume
+                </ModalHeader>
+                <ModalBody className='bg-white p-0 rounded-b-[4px]'>
+                    <div className="lg:flex gap-5 p-5">
+                        <div className='lg:w-6/12 border border-[#DADADA] rounded-[7px] p-4 pr-0'>
+                            <h3 className="text-[#151515] text-base font-medium pb-4">Select resume from the list</h3>
+                              <ul className='grid grid-cols-2 gap-5 resume_list_area'>
+                                 <li>
+                                    <input type="radio" name="test" id="cb1" />
+                                    <label for="cb1" className='bg-white border border-[#D5D5D5] p-4 rounded-[8px] mb-2'>
+                                       <Image src={resume01} alt="resume01" className='' />
+                                    </label>
+                                    <p className='text-[#000000] text-base font-semibold text-center pt-1'>Modern Template</p>
+                                 </li>
+                                 <li>
+                                    <input type="radio" name="test" id="cb2" />
+                                    <label for="cb2" className='bg-white border border-[#D5D5D5] p-4 rounded-[8px] mb-2'>
+                                       <Image src={resume01} alt="resume01" className='' />
+                                    </label>
+                                     <p className='text-[#000000] text-base font-semibold text-center'>Professional Template</p>
+                                 </li>
+                                 <li>
+                                    <input type="radio" name="test" id="cb3" />
+                                    <label for="cb3" className='bg-white border border-[#D5D5D5] p-4 rounded-[8px] mb-2'>
+                                       <Image src={resume01} alt="resume01" className='' />
+                                    </label>
+                                     <p className='text-[#000000] text-base font-semibold text-center'>Technical Template</p>
+                                 </li>
+                                 <li>
+                                    <input type="radio" name="test" id="cb4" />
+                                    <label for="cb4" className='bg-white border border-[#D5D5D5] p-4 rounded-[8px] mb-2'>
+                                       <Image src={resume01} alt="resume01" className='' />
+                                    </label>
+                                    <p className='text-[#000000] text-base font-semibold text-center'>Modern Template</p>
+                                 </li>
+                              </ul>
+                        </div>
+                        <div className='lg:w-6/12'>
+                            <div className="flex w-full items-center justify-center">
+                              <Label
+                              htmlFor="dropzone-file"
+                              className="resume_upload_box flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-white hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                              >
+                              <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                                 <BiImport className="text-[100px] text-[#92278F]" />
+                                 <p className="mb-2 text-xl text-[#92278F]">
+                                    Upload your resume
+                                 </p>
+                              </div>
+                              <FileInput id="dropzone-file" className="hidden" />
+                              </Label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="p-5 inset-shadow-xs">
+                       <button onClick={() => setOpenJobApplyModal(true)} className='bg-[#800080] hover:bg-[#151515] cursor-pointer px-10 text-[15px] leading-[45px] text-[#ffffff] font-semibold w-full text-center rounded-[7px]'>Apply Job</button>
+                    </div>
+                </ModalBody>
+            </Modal>
+      {/* add modal for apply job ends here */}
     </div>
   )
 }
