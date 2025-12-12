1fd9982b84545ea470cda0ff71de09547d2a1fdc
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-12-12 13:02:49 +0530

    new

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   output: 'standalone',
   // eslint: {
   //   ignoreDuringBuilds: true,
   // },
